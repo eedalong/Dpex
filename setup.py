23ff3bbb6d1d15a0e9cdd6f98acd08f40a90d592
mwx <mwx18@mails.tsinghua.edu.cn>
2021-06-02 16:16:48 +0800

    add email address into author_email in setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
       packages         = setuptools.find_packages(),
       version          = '1.0',
       author           = 'Xiulong Yuan, Zhan Lu, Zheng Zeng, Wenxuan Ma',
-      author_email     = 'yuanxl19@mails.tsinghua.edu.cn, lu-z18@mails.tsinghua.edu.cn, zengz17@mails.tsinghua.edu.cn',
+      author_email     = 'yuanxl19@mails.tsinghua.edu.cn, lu-z18@mails.tsinghua.edu.cn, zengz17@mails.tsinghua.edu.cn, mwx18@mails.tsinghua.edu.cn',
       url              = 'https://github.com/eedalong/DistributeDataLoader',
       license          = 'License :: OSI Approved :: MIT License',
       platforms        = 'Linux',
