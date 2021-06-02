7deaf32d9a7b6b293edbd882ae2ea9e177e73ea8
eedalong <ee_dalong@163.com>
2021-06-02 13:33:59 +0800

    add tests folder

---
+++ tests/test_bug_dataset.py
@@ -0,0 +1,25 @@
+from torch.utils.data import Dataset
+from dist_dataloader import dataloader
+import ray
+import time
+ray.init(address="auto")
+class BugDataset(Dataset):
+    def __init__(self):
+        super(BugDataset, self).__init__()
+        self.count = 30
+
+    def __getitem__(self, index):
+        self.count += 1
+        if self.count > 0:
+            a = 1 / 0
+        time.sleep(1)
+        return self.count
+
+    def __len__(self):
+        return self.count * 100
+
+bug_dataset = BugDataset()
+train_loader = dataloader.DistDataLoader(bug_dataset, distribute_mode=True, num_workers=10, batch_size=100, shuffle=True)
+for epoch in range(5):
+    for item in train_loader:
+        print(item)
